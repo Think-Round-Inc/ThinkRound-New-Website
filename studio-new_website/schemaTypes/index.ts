8832fed53c1151adf6f509c2a81db3112c3ee119
ssdv1 <160284219+ssdv1@users.noreply.github.com>
2026-01-04 19:07:54 -0500

    Add streamOfConsciousness to schemaTypes

--- studio-new_website/schemaTypes/index.ts
+++ studio-new_website/schemaTypes/index.ts
@@ -1,14 +1,17 @@
-import {postType} from './postType'
+import { postType } from './postType'
 import boardMember from './boardMember'
 import aboutUs from "./aboutUs"
 import partnerPast from "./aboutUsPartnerPast"
 import partnerCurrent from "./aboutUsPartner"
+import { streamOfConsciousness } from './streamOfConsciousness'
 
-export const schemaTypes = 
-[
-    boardMember,
-    postType,
-    aboutUs,
-    partnerPast,
-    partnerCurrent
-]
+export const schemaTypes =
+    [
+        boardMember,
+        postType,
+        aboutUs,
+        partnerPast,
+        partnerCurrent,
+        streamOfConsciousness
+    ]
+
