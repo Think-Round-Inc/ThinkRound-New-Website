b3cae2d19bcdc0b9ec75940eb4f12fbfa775d6cb
Varun98534 <varunsai260@gmail.com>
2026-01-27 03:19:50 -0600

    updated IAP

--- nextjs-new_website/src/app/programs/IAP/page.tsx
+++ nextjs-new_website/src/app/programs/IAP/page.tsx
@@ -1,6 +1,5 @@
 import { createClient } from 'next-sanity'
 import Image from 'next/image'
-import Link from 'next/link'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
