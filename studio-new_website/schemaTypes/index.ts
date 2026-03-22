ed76b7b7c6c0e37a465914463ed8da6bd1a5ea6e
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2026-03-22 03:17:28 -0700

    Update index.ts

--- studio-new_website/schemaTypes/index.ts
+++ studio-new_website/schemaTypes/index.ts
@@ -1,16 +1,27 @@
-import {postType} from './postType'
+import { postType } from './postType'
 import boardMember from './boardMember'
-import aboutUs from "./aboutUs"
-import partnerPast from "./aboutUsPartnerPast"
-import partnerCurrent from "./aboutUsPartner"
+import aboutUs from './aboutUs'
+import partnerPast from './aboutUsPartnerPast'
+import partnerCurrent from './aboutUsPartner'
+import { streamOfConsciousness } from './streamOfConsciousness'
+import donatePage from './donatePage'
+import blogs from './blogs'
+import pastExhibition from './pastExhibition'
+import iapPage from './iap'
+import classes from './classes'
 import turningTheTide from "./turningTheTideOfTrauma"
 
-export const schemaTypes = 
-[
-    boardMember,
-    postType,
-    aboutUs,
-    partnerPast,
-    partnerCurrent,
-    turningTheTide
+export const schemaTypes = [
+  boardMember,
+  postType,
+  aboutUs,
+  partnerPast,
+  partnerCurrent,
+  streamOfConsciousness,
+  donatePage,
+  blogs,
+  pastExhibition,
+  iapPage,
+  classes,
+  turningTheTide
 ]
