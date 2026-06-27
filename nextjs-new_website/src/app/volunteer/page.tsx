4aaae4683b96620871be5ce76a9310ec98f4a607
Ditjon Dafa <ditjon.dafa@outlook.com>
2026-06-27 11:31:15 +0200

    Improved the Volunteer page with the use of components.

--- nextjs-new_website/src/app/volunteer/page.tsx
+++ nextjs-new_website/src/app/volunteer/page.tsx
@@ -1,14 +1,15 @@
 import { client } from "@/sanity/client";
 import Navbar from "@/components/Navbar";
-import VolunteerFormClient from "@/app/volunteer/VolunteerFormClient";
+import VolunteerSubscribeFormClient from "@/components/volunteer-and-subscribe-page/VolunteerSubscribeFormClient";
 import { League_Spartan } from "next/font/google";
+import HeroSection from "@/components/volunteer-and-subscribe-page/HeroSection";
 
 const leagueSpartan = League_Spartan({ subsets: ["latin"] });
 
 export default async function VolunteerPage() {
-  const form = await client.fetch(
-    `*[_type == "volunteerForm"][0]{ firstName, lastName, email, dropdown, message }`,
-  );
+  // const form = await client.fetch(
+  //   `*[_type == "volunteerForm"][0]{ firstName, lastName, email, dropdown, message }`,
+  // );
 
   return (
     <main
@@ -21,37 +22,19 @@ export default async function VolunteerPage() {
       }}
     >
       <Navbar />
-
-      {/* Hero Section */}
-      <section className=" px-6 py-12  flex flex-col items-center justify-center   xl:mx-120 lg:mx-80 md:mx-35 sm:mx-10">
-        <h2
-          className="font-semibold mb-4 "
-          style={{
-            fontSize: "clamp(1.8em, 5vw, 3.5em)",
-            fontWeight: 300,
-            fontFamily: leagueSpartan.style.fontFamily,
-            color: "#000",
-          }}
-        >
-          Interested in volunteering with us?
-        </h2>
-        <p
-          style={{
-            fontSize: "clamp(1em, 4vw, 1.8em)",
-            lineHeight: "1.5em",
-          }}
-        >
-          Our projects provide a great opportunity to learn and hone your
-          creative skillsets while working on real projects.
-        </p>
-      </section>
+      <HeroSection
+        heading2="Interested in volunteering with us?"
+        paragraph="  Our projects provide a great opportunity to learn and hone your
+          creative skillsets while working on real projects."
+      />
 
       {/* Form Section */}
       <section
         className=" px-6 py-12 xl:mx-120 lg:mx-80 md:mx-35 sm:mx-10"
         style={{ fontFamily: leagueSpartan.style.fontFamily }}
       >
-        <VolunteerFormClient initialForm={form} />
+        {/* <VolunteerFormClient initialForm={form} /> */}
+        <VolunteerSubscribeFormClient id="volunteer" />
       </section>
     </main>
   );
