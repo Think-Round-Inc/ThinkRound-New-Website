9d6a40a472bbfa215145ffa0b46d6232fe0b423b
Robert Penrod <robertpenrod42@gmail.com>
2026-08-25 19:07:52 -0400

    Styling Updates

--- nextjs-new_website/src/app/think_round_fine_arts/virtual_art_exhibitions/page.tsx
+++ nextjs-new_website/src/app/think_round_fine_arts/virtual_art_exhibitions/page.tsx
@@ -1,7 +1,8 @@
 import Navbar from "@/components/Navbar";
-import Image from 'next/image';
-import { PortableText } from '@portabletext/react';
 import { client, urlFor } from "@/sanity/client";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+import { TypedObject } from "sanity";
 
 interface VirtualExhibitionCard {
     exhibitionTitle: string;
@@ -16,11 +17,11 @@ interface VirtualExhibitionsData {
         alt?: string;
         hotspot?: { x: number; y: number };
     };
-    description: any[];
+    description: TypedObject[];
     virtualExhibitions: VirtualExhibitionCard[];
 }
 
-const options = { next: { revalidate: 30 } }
+const options = { next: { revalidate: 30 } };
 
 const QUERY = `*[_type == "virtualExhibitions3D"][0]{
   mainTitle,
@@ -28,21 +29,16 @@ const QUERY = `*[_type == "virtualExhibitions3D"][0]{
   image,
   description,
   virtualExhibitions
-}`
+}`;
 
 async function GetVirtualExhibitionsData(): Promise<VirtualExhibitionsData | null> {
-    return client.fetch<VirtualExhibitionsData>(
-        QUERY,
-        {},
-        options
-    );
+    return client.fetch<VirtualExhibitionsData>(QUERY, {}, options);
 }
 
 export default async function VirtualArtiExhibitionsPage() {
-    
     const virtualExhibitionsData = await GetVirtualExhibitionsData();
 
-    if(!virtualExhibitionsData) {
+    if (!virtualExhibitionsData) {
         return (
             <>
                 <Navbar />
@@ -56,44 +52,59 @@ export default async function VirtualArtiExhibitionsPage() {
     return (
         <>
             <Navbar />
-            <main className='min-h-screen bg-white px-6 py-16'>
+            <main className='min-h-screen bg-white px-6 py-16 '>
                 <div className='max-w-[1400px] mx-auto'>
                     {/* Page Title */}
-                    <h1 className='text-4xl md:text-5xl font-light uppercase leading-tight tracking-tight text-black text-center mb-16'>
-                       {virtualExhibitionsData.mainTitle}
+                    <h1 className='text-4xl md:text-5xl font-bold uppercase leading-tight tracking-tight text-black text-center mb-2'>
+                        {virtualExhibitionsData.mainTitle}
                     </h1>
                     {/* Page Sub Title */}
-                    <p className='text-2xl md:text-2xl font-light leading-tight tracking-tight text-black text-center mb-16'>
+                    <p className='text-2xl md:text-2xl font-bold leading-tight tracking-tight text-black text-center mb-4'>
                         {virtualExhibitionsData?.subTitle}
                     </p>
                     {/* Image */}
-                    <Image
-                        src={urlFor(virtualExhibitionsData.image).width(1200).url()}
-                        alt={virtualExhibitionsData.mainTitle}
-                        width={1200}
-                        height={675}
-                        className="mb-6 rounded-lg"
-                    />
-                    {/* Description */}
-                    <div className="mb-10">
-                        <PortableText value={virtualExhibitionsData.description} />
-                    </div>
-                    {/* Virtual Exhibitions */}
-                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
-                        {virtualExhibitionsData.virtualExhibitions.map((exhibition: VirtualExhibitionCard, index: number) => (
-                            <div key={index}>
-                                <h3 className="text-2xl font-semibold mb-4">
-                                    {exhibition.exhibitionTitle}
-                                </h3>
-                                <iframe
-                                    src={exhibition.exhibitionUrl}
-                                    className="w-full min-h-[600px] rounded-lg border"
-                                    allow="fullscreen; xr-spatial-tracking"
-                                    allowFullScreen
-                                    loading="lazy"
+                    <div className='grid grid-cols-12 mb-4'>
+                        <div className='col-span-12 md:col-span-6 md:col-start-4'>
+                            <Image
+                                src={urlFor(virtualExhibitionsData.image)
+                                    .width(1200)
+                                    .url()}
+                                alt={virtualExhibitionsData.mainTitle}
+                                width={0}
+                                height={0}
+                                className='mb-6 rounded-lg mx-auto'
+                                sizes='100vw'
+                                style={{ width: "100%", height: "auto" }}
+                            />
+                            {/* Description */}
+                            <div className='mb-10 text-[#777777] text-[12px]'>
+                                <PortableText
+                                    value={virtualExhibitionsData.description}
                                 />
                             </div>
-                        ))}
+                        </div>
+                    </div>
+                    {/* Virtual Exhibitions */}
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+                        {virtualExhibitionsData.virtualExhibitions.map(
+                            (
+                                exhibition: VirtualExhibitionCard,
+                                index: number,
+                            ) => (
+                                <div key={index}>
+                                    <h3 className='text-4xl font-semibold mb-4 text-[#424242]'>
+                                        {exhibition.exhibitionTitle}
+                                    </h3>
+                                    <iframe
+                                        src={exhibition.exhibitionUrl}
+                                        className='w-full min-h-[600px] rounded-lg border'
+                                        allow='fullscreen; xr-spatial-tracking'
+                                        allowFullScreen
+                                        loading='lazy'
+                                    />
+                                </div>
+                            ),
+                        )}
                     </div>
                 </div>
             </main>
