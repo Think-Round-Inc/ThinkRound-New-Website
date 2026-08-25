6111278fa05faacf40e7b9505fe70f2e96b08637
Robert Penrod <robertpenrod42@gmail.com>
2026-08-25 09:47:06 -0400

    Page Layout

--- nextjs-new_website/src/app/think_round_fine_arts/virtual_art_exhibitions/page.tsx
+++ nextjs-new_website/src/app/think_round_fine_arts/virtual_art_exhibitions/page.tsx
@@ -1,3 +1,20 @@
+import Navbar from "@/components/Navbar";
+
 export default async function VirtualArtiExhibitionsPage() {
-    return <>Virtual Art Exhibitions</>;
+    return (
+        <>
+            <Navbar />
+            <main className='min-h-screen bg-white px-6 py-16'>
+                <div className='max-w-[1400px] mx-auto'>
+                    {/* Page Title */}
+                    <h1 className='text-4xl md:text-5xl font-light uppercase leading-tight tracking-tight text-black text-center mb-16'>
+                        Virtual Art Exhibitions
+                    </h1>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
+                        {/* TODO: Exhibition Components here */}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
