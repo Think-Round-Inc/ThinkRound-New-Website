30fd77dba4de00d41e0d7436fe78796ddd5c58b7
Ditjon Dafa <ditjon.dafa@outlook.com>
2026-09-07 11:45:22 +0200

    Changed the text size of the search term.

--- nextjs-new_website/src/components/Navbar.tsx
+++ nextjs-new_website/src/components/Navbar.tsx
@@ -311,7 +311,7 @@ export default function Navbar() {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               onKeyDown={handleSearchKeyPress}
-              className="bg-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:border-white placeholder-gray-500 text-gray-800 text-md transition-all duration-200 pr-10"
+              className="bg-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:border-white placeholder-gray-500 text-gray-800 text-lg transition-all duration-200 pr-10"
               placeholder="SEARCH"
             />
             <button
